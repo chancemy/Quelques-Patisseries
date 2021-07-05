e6958b350123c95e0e5fcd129a0442a02dd45357
poiu60177 <82428714+poiu60177@users.noreply.github.com>
2021-07-05 14:37:01 +0800

    210705-修復檔案

---
+++ js/frontpage/index.js
@@ -0,0 +1,32 @@
+
+var frontNav = document.querySelector('nav');
+const banner = document.querySelector('.banner');
+
+window.addEventListener('scroll', function (e) {
+    let st = this.scrollY;
+    let bannerHeight = banner.offsetHeight;
+
+    if (st > bannerHeight) {
+        frontNav.classList.remove('front-nav');
+    } else {
+        frontNav.classList.add('front-nav');
+    }
+
+});
+
+
+setTimeout(showPage, 1700);
+
+function showPage() {
+    var loadingPage = document.querySelector('.loading');
+    var body = document.querySelector('body');
+    loadingPage.classList.add('hidden');
+
+    setTimeout(function () {
+        loadingPage.classList.add('display-none');
+        body.classList.remove('loading-animate');
+    }, 500);
+
+}
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
